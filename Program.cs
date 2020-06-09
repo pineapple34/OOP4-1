bc67253b7a6d5a346f59a490e41b73d1d7d6212e
pineapple34 <pineapple6934@gmail.com>
2020-06-09 16:44:50 +0500

    Initial commit

---
+++ Program.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace OOP4_1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Health health = new Health();
+            while (true)
+            {
+                Console.Clear();
+                Console.WriteLine("Температура - " + health.Temperature);
+                Console.WriteLine("Новое значение - ");
+                int.TryParse(Console.ReadLine(), out int newTemp);
+                health.Temperature = newTemp;
+            }
+        }
+    }
+
+    class Health
+    {
+        private int temperature;
+        public int Temperature
+        {
+            get
+            {
+                return temperature;
+            }
+            set
+            {
+                if (value >= 34 && value <= 41) temperature = value;
+            }
+        }
+        public Health()
+        {
+            this.temperature = 36;
+        }
+    }
+}
